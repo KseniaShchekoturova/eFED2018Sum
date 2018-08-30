dbd3fb2e7130a578eaf741e4922bc42973c282c0
KseniaShchekoturova <igoshevakp@gmail.com>
2018-08-30 10:15:23 +0400

    homework Quality code

--- js_basics/array_functions.js
+++ js_basics/array_functions.js
@@ -1,114 +1,114 @@
 // 4.	Диапазон
 function getDiapason(start, finish, step) {
-  if ((start == undefined) | (finish == undefined) | (step == 0)) {
-    return "bad options";
-  }
+    if ((start == undefined) | (finish == undefined) | (step == 0)) {
+        return "bad options";
+    }
 
-  if (step == undefined) {
-    step = 1;
-  }
+    if (step == undefined) {
+        step = 1;
+    }
 
-  var diapason = [];
+    var diapason = [];
 
-  if (start == finish) {
-    diapason.push(start);
+    if (start == finish) {
+        diapason.push(start);
+        return diapason;
+    }
+
+    if ((finish - start) * step < 0) {
+        step = -step;
+    }
+
+    if (start < finish) {
+        while (start <= finish) {
+            diapason.push(start);
+            start += step;
+        }
+    } else {
+        while (start >= finish) {
+            diapason.push(start);
+            start += step;
+        }
+    }
+
     return diapason;
-  }
-
-  if ((finish - start) * step < 0) {
-    step = -step;
-  }
-
-  if (start < finish) {
-    while (start <= finish) {
-      diapason.push(start);
-      start += step;
-    }
-  } else {
-    while (start >= finish) {
-      diapason.push(start);
-      start += step;
-    }
-  }
-
-  return diapason;
 }
 
 // 5.	Наоборот
 function reverseArray(array) {
-  var i = 0;
-  var length = array.length;
-  var reversedArray = [];
+    var i = 0;
+    var length = array.length;
+    var reversedArray = [];
 
-  while (i < length) {
-    reversedArray[i] = array[length - i - 1];
-    i++;
-  }
+    while (i < length) {
+        reversedArray[i] = array[length - i - 1];
+        i++;
+    }
 
-  return reversedArray;
+    return reversedArray;
 }
 
 function reverseArrayInPlace(array) {
-  var i = 0;
-  var pastValue;
-  var length = array.length;
+    var i = 0;
+    var pastValue;
+    var length = array.length;
 
-  while (i < length / 2) {
-    pastValue = array[i];
-    array[i] = array[length - i - 1];
-    array[length - i - 1] = pastValue;
-    i++;
-  }
+    while (i < length / 2) {
+        pastValue = array[i];
+        array[i] = array[length - i - 1];
+        array[length - i - 1] = pastValue;
+        i++;
+    }
 
-  return array;
+    return array;
 }
 
 // 6.	Свертка
 function mergeArrays(...arrays) {
-  var totalArray = [];
-  var mergedArray = [];
+    var totalArray = [];
+    var mergedArray = [];
 
-  for (var i = 0; i < arrays.length; i++) {
-    totalArray = totalArray.concat(arrays[i]);
-  }
-
-  for (var i = 0; i < totalArray.length; i++) {
-    var item = totalArray[i];
-    if (getPositionInArray(mergedArray, item) == -1) {
-      mergedArray.push(item);
+    for (var i = 0; i < arrays.length; i++) {
+        totalArray = totalArray.concat(arrays[i]);
     }
-  }
 
-  return mergedArray;
+    for (var i = 0; i < totalArray.length; i++) {
+        var item = totalArray[i];
+        if (getPositionInArray(mergedArray, item) == -1) {
+            mergedArray.push(item);
+        }
+    }
+
+    return mergedArray;
 }
 
 function getPositionInArray(array, value) {
-  for (var i = 0; i < array.length; i++) {
-    if (array[i] === value) {
-      return i;
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] === value) {
+            return i;
+        }
     }
-  }
 
-  return -1;
+    return -1;
 }
 
 // 7.	Every и some
 function isEveryNaN(array) {
-  for (var i = 0; i < array.length; i++) {
-    if (!isNaN(array[i])) {
-      return false;
+    for (var i = 0; i < array.length; i++) {
+        if (!isNaN(array[i])) {
+            return false;
+        }
     }
-  }
 
-  return true;
+    return true;
 }
 
 function isSomeNaN(array) {
-  for (var i = 0; i < array.length; i++) {
-    if (isNaN(array[i])) {
-      return true;
+    for (var i = 0; i < array.length; i++) {
+        if (isNaN(array[i])) {
+            return true;
+        }
     }
-  }
 
-  return false;
+    return false;
 }
